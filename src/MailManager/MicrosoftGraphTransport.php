ebc43ee4ff1146dde331dccf873fc69850856c03
DelaneyArkICT <89026864+DelaneyArkICT@users.noreply.github.com>
2025-06-26 01:46:45 +0100

    Update MicrosoftGraphTransport.php

--- src/MailManager/MicrosoftGraphTransport.php
+++ src/MailManager/MicrosoftGraphTransport.php
@@ -6,19 +6,22 @@ use LLoadout\Microsoftgraph\Mail;
 use Symfony\Component\Mailer\Envelope;
 use Symfony\Component\Mailer\SentMessage;
 use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\MessageConverter;
 use Symfony\Component\Mime\RawMessage;
 
 class MicrosoftGraphTransport implements TransportInterface
 {
     public function send(RawMessage $message, Envelope $envelope = null): ?SentMessage
     {
-        app(Mail::class)->sendMail($message);
+        $email = MessageConverter::toEmail($message);
+        
+        app(Mail::class)->sendMail($email);
 
         return new SentMessage($message, $envelope);
     }
 
     public function __toString(): string
     {
-        return '';
+        return 'microsoftgraph://';
     }
 }
